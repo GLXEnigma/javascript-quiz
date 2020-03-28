a0442aa6674b5265ecf28eddae4e018923aecef4
GLX Enigma <tyrussell2112@gmail.com>
2020-03-27 21:11:44 -0600

    adding everything I added with no internet connection hoping for the best.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,20 @@
 
+const options=document.querySelector(".options").children;
 const questionNumberSpan=document.querySelector(".question-num-value")
 const totalQuestionSpan=document.querySelector(".total-questions");
+const correctAnswerSpan=document.querySelector(".correct-answers");
+const totalQuestions=document.querySelector(".total-questions2");
+const percentage=document.querySelector(".percentage");
 const question=document.querySelector(".question");
 const op1=document.querySelector(".option1");
 const op2=document.querySelector(".option2");
 const op3=document.querySelector(".option3");
 const op4=document.querySelector(".option4");
-let questionIndex
+let questionIndex;
 let index=0;
-
+let myArray=[];
+let myArr=[];
+let score=0;
 
 
 //questions
@@ -50,14 +56,102 @@ function load(){
     index++;
 }
 
-function randomQuestion(){
-    let randomNumber=Math.floor(Math.random()*questions.length);
-    questionIndex=randomNumber;
-    console.log(questionIndex)
-    load();
+
+function check(element){
+    if(element.id==questions[questionIndex].answer){
+        element.classList.add("correct");
+        score++;
+        //console.log(score);
+    }else{
+        element.classList.add("incorrect");
+        //console.log("incorrect");
+    }
+
+    disabledOptions()
 }
 
+function disabledOptions(){
+    for(let i=0; i<options.length; i++){
+        options[i].classList.add("disabled");
+        if(options[i].id==questions[questionIndex].answer){
+            options[i].classList.add("correct");
+            
+        }
+    }
+}
+function enableOptions(){
+    for(let i=0; i<options.length; i++){
+        options[i].classList.remove("disabled", "correct", "incorrect");
+    }
+}
+
+function validate(){
+    if(!options[0].classList.contains("disabled")){
+        alert("Please select one option")
+    }else{
+        enableOptions();
+        randomQuestion();
+    }
+}
+function next(){
+    validate();
+}
+
+
+
+function randomQuestion(){
+    let randomNumber=Math.floor(Math.random()*questions.length);
+    let hitDuplicate=0;
+    if(index==questions.length){
+        console.log("Quiz Over");
+        quizComplete();
+    }else{   
+        if(myArray.length>0){
+            for(let i=0; i<myArray.length; i++){
+                if(myArray[i]==randomNumber){
+                    hitDuplicate=1;
+                    break;
+                }
+            }
+            if(hitDuplicate==1){
+                randomQuestion();
+            }else{
+                questionIndex=randomNumber;
+                load();
+                myArr.push(questionIndex);
+            }
+        }
+        if(myArray.length==0){
+            questionIndex=randomNumber;
+            load();
+            myArr.push(questionIndex);
+        }
+        console.log("myArr:" + myArr);
+
+        myArray.push(randomNumber);
+        //console.log(questionIndex)
+        
+    }
+}
+
+function quizComplete(){
+    document.querySelector(".quiz-complete").classList.add("show");
+    correctAnswerSpan.innerHTML=score;
+    totalQuestions.innerHTML=questions.length;
+    percentage.innerHTML=(score/questions.length)*100 + "%";
+}
+
+function tryAgain(){
+    window.location.reload();
+}
 
 window.onload=function(){
     this.randomQuestion();
 }
+
+
+// this is where i need to add the timer
+function timer(){
+
+}
+
